7140470acbf741d45c1521c10b1f8a3c9589c130
moonbyeongmin <qudalsrnt3x@gmail.com>
2025-05-07 15:03:40 +0900

    Feat: 프로젝트 초기 설정

---
+++ src/test/java/com/frankit/assignment/FrankitAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankit.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrankitAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
